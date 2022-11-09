a45d448d5fc7a17aef9bfe9424b93fbe364258dd
sujith <sujith.cs20@bitsathy.ac.in>
2022-11-09 18:50:23 +0530

    third

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+const days=document.querySelector("#days");
+const hours=document.querySelector("#hours");
+const minutes=document.querySelector("#minutes");
+const seconds=document.querySelector("#seconds");
 
- function updateTime(){
+
+
+function updateTime(){
 const currentYear=new Date().getFullYear();
 const newYear=new Date(`january 1 ${currentYear+1} 00:00:00`);
 const currentDate=new Date();
@@ -9,6 +15,10 @@ const h=Math.floor((diff/1000/60/60)%24);
 const m=Math.floor((diff/1000/60%60));
 const s=Math.floor((diff/1000%60));
 //console.log(d+" "+h+" "+m+" "+s)
+days.innerHTML=d<10?"0"+d:d;
+hours.innerHTML=h<10?"0"+h:h;
+minutes.innerHTML=m<10?"0"+m:m;
+seconds.innerHTML=s<10?"0"+s:s;
  }
  
  setInterval(updateTime,1000);
